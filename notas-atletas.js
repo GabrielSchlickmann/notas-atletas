ba960decd6b545054518789f82b2f06b86b3e036
GabrielSchlickmann <gabrielschlickmann@gmail.com>
2025-01-01 17:27:36 -0300

    notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,1 +1,36 @@
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
 
+   atletas.forEach(atletaAtual => {
+    atletaAtual.notas.sort((a, b) => a - b);
+    
+    let notasComputadas = atletaAtual.notas.slice(1, atletaAtual.notas.length - 1);
+
+    let soma = 0
+    notasComputadas.forEach(notaAtual => {
+        soma = soma + notaAtual;
+        
+    });
+    let denominador = atletaAtual.notas.length - 2;
+    let media = soma / denominador;
+    console.log(`Atleta: ${atletaAtual.nome}`);
+    console.log(`Notas Obtidas: ${atletaAtual.notas}`)
+    console.log(`Média Válida: ${media}`)
+    console.log();
+   });
